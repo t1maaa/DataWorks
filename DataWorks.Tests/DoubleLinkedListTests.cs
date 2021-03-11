9b18789e2f09faeb6922eb4d39431d75498d622c
t1maaa <t1maaa@mail.ru>
2021-03-11 14:28:59 +0700

    DoubleLinkedListTests.cs

--- DataWorks.Tests/DoubleLinkedListTests.cs
+++ DataWorks.Tests/DoubleLinkedListTests.cs
@@ -1,14 +1,47 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
 using Xunit;
 
 namespace DataWorks.Tests
 {
-    public class Tests
+    public class DoubleLinkedListTests
     {
+        private DoubleLinkedList<int> list = new DoubleLinkedList<int>();
+
         [Fact]
-        public void Test1()
+        public void AddFirst()
         {
-            Assert.True(true);
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            Assert.Equal("3, 2, 1", list.ToString());
+        }
+
+        [Fact]
+        public void AddLast()
+        {
+            list.AddLast(0);
+            list.AddLast(-1);
+            list.AddLast(-2);
+            Assert.Equal("0, -1, -2", list.ToString());
+        }
+
+        [Fact]
+        public void Reverse()
+        {
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            list.AddLast(0);
+            list.AddLast(-1);
+            list.AddLast(-2);
+            
+            list.Reverse();
+            Assert.Equal("-2, -1, 0, 1, 2, 3", list.ToString());
+            
+            list.Reverse();
+            Assert.Equal("3, 2, 1, 0, -1, -2", list.ToString());
         }
     }
 }
