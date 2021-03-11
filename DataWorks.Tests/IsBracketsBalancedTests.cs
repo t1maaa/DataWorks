c4bf174865affea4937f8f56a4aaad9a8d1253cc
t1maaa <t1maaa@mail.ru>
2021-03-11 15:05:28 +0700

    refactor IsBracketsBalancedTests.cs + added empty string case

--- DataWorks.Tests/IsBracketsBalancedTests.cs
+++ DataWorks.Tests/IsBracketsBalancedTests.cs
@@ -6,15 +6,25 @@ namespace DataWorks.Tests
     public class IsBracketsBalancedTests
     {
         [Fact]
-        public void Tests()
+        public void Should_Return_True()
         {
             Assert.True(DataWorks.IsBracketsBalanced("()"));
             Assert.True(DataWorks.IsBracketsBalanced("(())"));
             Assert.True(DataWorks.IsBracketsBalanced("(())"));
             Assert.True(DataWorks.IsBracketsBalanced("()()"));
-            
+        }
+
+        [Fact]
+        public void Should_Return_False()
+        {
             Assert.False(DataWorks.IsBracketsBalanced("()("));
             Assert.False(DataWorks.IsBracketsBalanced(")()"));
+        }
+
+        [Fact]
+        public void Should_Throw_Exception()
+        {
+            Assert.Throws<ArgumentException>(() => DataWorks.IsBracketsBalanced(""));
         }
         
     }
