4ca15c28fdcb43f16b065eb0a6c2419eabcc6c2a
t1maaa <t1maaa@mail.ru>
2021-03-11 14:30:28 +0700

    RomanToInt and IsBracketsBalanced func

---
+++ DataWorks.Tests/src/DataWorks.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataWorks.Tests
+{
+    public static class DataWorks
+    {
+        #region RomanToInt
+
+        public static int RomanToInt(string romanNumber)
+        {
+            if (string.IsNullOrWhiteSpace(romanNumber) || int.TryParse(romanNumber, out _)) return 0;
+            var romanValues = new Dictionary<char, int>
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+
+            int number = 0;
+            
+            for (var i = 0; i < romanNumber.Length; i++)
+            {
+                if (romanValues.TryGetValue(romanNumber[i], out var curVal))
+                {
+                    if ( (i + 1 <= romanNumber.Length - 1) && romanValues.TryGetValue(romanNumber[i + 1], out var nextVal))
+                    {
+                        if (curVal >= nextVal)
+                            number += curVal;
+                        else
+                            number -= curVal;
+                    }
+                    else
+                        number += curVal;
+                }
+            }
+            return number;
+        }
+
+        #endregion
+
+        #region IsBracketsBalanced
+
+        public static bool IsBracketsBalanced(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) throw new ArgumentException("Empty string");
+
+            var leftBracket = 0;
+            var rightBracket = 0;
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        leftBracket++;
+                        break;
+                    case ')':
+                        rightBracket++;
+                        break;
+                }
+
+                if (rightBracket > leftBracket) return false;
+            }
+            return rightBracket == leftBracket;
+        }
+
+        #endregion
+    }
+}
